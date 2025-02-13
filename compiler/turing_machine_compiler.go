1489c251b4fb02c91737fb09e852a06223e8ee83
kTrzcinskii <kt.trzcinski@gmail.com>
2025-02-13 12:20:06 +0100

    Process accepting states in tm compiler

--- compiler/turing_machine_compiler.go
+++ compiler/turing_machine_compiler.go
@@ -22,6 +22,10 @@ func (tm *TuringMachineCompiler) Compile() (automata.Automata, error) {
 	if err != nil {
 		return zero, err
 	}
+	err = tm.processAcceptingStates(states)
+	if err != nil {
+		return zero, err
+	}
 	return automata.TuringMachine{States: states, InitialState: initialState}, nil
 }
 
@@ -82,4 +86,23 @@ func (tm *TuringMachineCompiler) processInitialState(states map[string]automata.
 	}
 
 	return initialState.Value, nil
+}
+
+func (tm *TuringMachineCompiler) processAcceptingStates(states map[string]automata.State) error {
+	for !tm.isAtEnd() {
+		t := tm.advance()
+		switch t.Type {
+		case lexer.SemicolonToken:
+			return nil
+		case lexer.StateToken:
+			name := t.Value
+			if _, ok := states[name]; !ok {
+				return fmt.Errorf("state %s not found, any accepting state must be defined in state list", name)
+			}
+			states[name] = automata.State{Name: name, Accepting: true}
+		default:
+			return fmt.Errorf("invalid token type, expected: %s or %s, got: %s", lexer.StateToken.String(), lexer.SemicolonToken.String(), t.Type.String())
+		}
+	}
+	return errors.New("missing ';' at the end of accepting states section")
 }
